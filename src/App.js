4ece3623422976244c7cfc243be7e12d91b0efc6
RalphHalabi <ralphhalabi22@gmail.com>
2024-03-06 00:28:01 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+   <>
       <Header />
-    </div>
+
+     <main className='main'>
+<Home />
+     </main>
+      </>
   );
 }
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import Spline from "@splinetool/react-spline";
+import Social from "./Social";
+import Data from "./Data";
 import "./Home.css";
 
-function Home() {
-    return (
-        <div className="Home">Home section</div>
-    )
-}
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
+
+          <div className="home__img"></div>
+
+          <Data />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
