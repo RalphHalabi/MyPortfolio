aefc2624c08cbc82b948b92bf0acab5b2c05c73a
RalphHalabi <ralphhalabi22@gmail.com>
2024-03-06 01:54:06 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+import "./App.css";
+import About from "./components/About/About";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-   <>
+    <>
       <Header />
 
-     <main className='main'>
-<Home />
-     </main>
-      </>
+      <main className="main">
+        <Home />
+        <About />
+      </main>
+    </>
   );
 }
 
