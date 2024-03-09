360451cb6f8b62de307b3548f537eee52dee87fb
ralph halabi <ralphhalabi22@gmail.com>
2024-03-09 14:52:36 +0200

    fixes

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -114,7 +114,7 @@ const Qualification = () => {
                 <span className="qualification__subtitle">Digital Splash</span>
                 <div className="qualification__calendar">
                   <i className="uil uil-calendar-alt"></i>
-                  2022-2023
+                  2023-2024
                 </div>
               </div>
 
@@ -123,6 +123,24 @@ const Qualification = () => {
                 <span className="qualification__line"></span>
               </div>
             </div>
+
+            {/* <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Team Leader</h3>
+                <span className="qualification__subtitle">Crepaway</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i>
+                  2017 - 2022 
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
